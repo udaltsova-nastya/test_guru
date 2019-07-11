8b24d483b891e45274405347e5e72c0366cd1cb6
Nastya Udaltsova <udaltsova.nastya@gmail.com>
2019-07-11 09:40:46 +0300

    Associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,9 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  has_many :user_tests
+  has_many :users, through: :user_tests
+  belongs_to :author, class_name: "User"
 
   def self.by_category(title)
     Category.by_title(title).tests.order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :user_tests
   has_many :tests, through: :user_tests
+  has_many :author_tests, class_name: "Test", foreign_key: :author_id
 
   def find_tests_by_level(level)
     tests.where(level: level)
